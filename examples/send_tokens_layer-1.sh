35005592d15cd7daecd71b4da50f6fb61c7e7c79
boyuchen0224 <boyuchen0224@gmail.com>
2020-05-09 15:25:55 +0800

    Add script for sending token

---
+++ examples/send_tokens_layer-1.sh
@@ -0,0 +1,11 @@
+username=$1
+tokenAmount=$2
+
+for((i=0;i<${tokenAmount};i++));
+do
+	curl http://3.87.137.58:8888/send_token \
+			-X POST \
+      -H 'Content-Type: application/json' \
+      -H 'X-API-key: 9jhy765ae128e45629ihbn292b2b3f19084ijygv' \
+      -d '{"sen": "cb", "rev": "'${username}'", "method": "1", "description": "SCU Token", "txn":""}';
+done
