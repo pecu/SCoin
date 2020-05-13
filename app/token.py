e41dc54f08a331300073c05727f2abba7d3478b2
boyuchen0224 <boyuchen0224@gmail.com>
2020-05-14 00:42:06 +0800

    Add balacne response info

--- app/token.py
+++ app/token.py
@@ -6,24 +6,28 @@ import os
 from app.did import DID
 from app.rsa import encrypt_with_pub_key, decrypt_with_pri_key
 from app.blockchain.tangle import send_transfer, get_txn_hash_from_bundle, \
-        find_transaction_message, generate_new_address, get_account_data
+    find_transaction_message, generate_new_address, get_account_data
 from app.auth import check_api_key
 
 PATH_ACCOUNT = "./accounts/"
 
+
 def new_seed(user):
-    seed = ''.join(random.choice(string.ascii_uppercase + "9") for _ in range(81))
+    seed = ''.join(random.choice(string.ascii_uppercase + "9")
+                   for _ in range(81))
     with open(PATH_ACCOUNT + user + "/seed.txt", 'a') as outfile:
         outfile.write(seed + "\n")
 
-    return seed 
+    return seed
+
 
 def load_token_json_obj():
     obj_token = {}
     with open("credential/token.json", 'r') as outfile:
         obj_token = json.load(outfile)
-        
+
     return obj_token
+
 
 def get_txn_enseed(txn_hash):
     enseed = ""
@@ -34,6 +38,7 @@ def get_txn_enseed(txn_hash):
 
     return enseed
 
+
 def check_token_in_history(user, txn):
     with open(PATH_ACCOUNT + user + "/history.txt", 'r') as outfile:
         list_balance = outfile.read().splitlines()
@@ -42,10 +47,11 @@ def check_token_in_history(user, txn):
         else:
             return False
 
+
 def layer_to_layer(api_key, data):
     if data["txn"] != "":
         if check_token_in_history(data["sen"], data["txn"]) == False:
-            return {"status":"error","msg":"No this token in wallet"}
+            return {"status": "error", "msg": "No this token in wallet"}
 
     # Load basic token cred
     cred = load_token_json_obj()
@@ -60,7 +66,7 @@ def layer_to_layer(api_key, data):
 
     # Check receiver exist
     if not os.path.isdir("accounts/" + data["rev"]):
-        return {"status":"error", "msg":"Receiver not exist."}
+        return {"status": "error", "msg": "Receiver not exist."}
 
     # Get/Set receiver DID ID
     id_receiver = did.get_DID_from_username(data["rev"])
@@ -82,7 +88,7 @@ def layer_to_layer(api_key, data):
 
     # Encrypt seed
     enseed = encrypt_with_pub_key(receiver_pub_key, seed)
-        
+
     # Set enseed
     cred["enseed"] = enseed
 
@@ -90,15 +96,15 @@ def layer_to_layer(api_key, data):
     address = generate_new_address(seed)
     cred["address"] = address
 
-    ## Send to Tangle
+    # Send to Tangle
     hash_bundle = send_transfer(cred, address, seed)
     hash_txn = get_txn_hash_from_bundle(hash_bundle)
 
-    ## Save to history
+    # Save to history
     with open(PATH_ACCOUNT + data["rev"] + "/history.txt", 'a') as outfile:
         outfile.write(hash_txn + "\n")
 
-    ## Update history list
+    # Update history list
     if data["txn"] != "" or data["method"] != "1":
         list_balance = []
         with open(PATH_ACCOUNT + data["sen"] + "/history.txt", 'r') as outfile:
@@ -109,42 +115,61 @@ def layer_to_layer(api_key, data):
                 print("No " + data["txn"] + " in history.")
 
         with open(PATH_ACCOUNT + data["sen"] + "/history.txt", 'w') as outfile:
-             for listitem in list_balance:
-                 outfile.write('%s\n' % listitem)
+            for listitem in list_balance:
+                outfile.write('%s\n' % listitem)
 
     return hash_txn
 
+
 def get_user_balance(user):
     # Check user exist
+    balanceInfo = {
+        'balance': None,
+        'tx': [],
+        'status': 'Success',
+    }
+
     if not os.path.isdir(PATH_ACCOUNT + user):
-        return ""#{"status":"error", "msg":"User not exist."}
+        balanceInfo['status'] = 'Fail'
+        balanceInfo['error'] = '''User doesn't not exist'''
+
+        return balanceInfo
 
     # Check history file exist
     if not os.path.isfile(PATH_ACCOUNT + user + "/history.txt"):
-        return ""#""
+        balanceInfo['balance'] = 0
+
+        return balanceInfo
 
     with open(PATH_ACCOUNT + user + "/history.txt", 'r') as outfile:
-        return outfile.read()[:-1]
+        for i, l in enumerate(outfile):
+            balanceInfo['tx'].append(l)
+
+        balanceInfo['balance'] = i + 1
+
+        return balanceInfo
+
 
 def check_token_valid(user, api_key, data):
-    # Get transaction message field    
+    # Get transaction message field
     msg_txn = find_transaction_message(data["token"])
     obj_msg = json.loads(msg_txn)
     enseed = obj_msg["enseed"]
- 
+
     # Decrypt seed
     seed = decrypt_with_pri_key(user, api_key, enseed)
 
     # Verify address
-    # start = time.time() 
+    # start = time.time()
     account_info = get_account_data(seed)
     # print("account_info : " + str(account_info))
     # end = time.time()
     # print("Duration of get_account_data function: " + str(end - start))
-    
+
     latest_address = ""
     try:
-        latest_address = str(account_info["addresses"][len(account_info["addresses"])-1])
+        latest_address = str(
+            account_info["addresses"][len(account_info["addresses"])-1])
     except:
         return False
 
@@ -153,29 +178,30 @@ def check_token_valid(user, api_key, data):
     else:
         return False
 
+
 def snapshot(api_key, data):
     new_token = data["token"]
 
-    # Get transaction message field    
+    # Get transaction message field
     msg_txn = find_transaction_message(data["token"])
     obj_msg = json.loads(msg_txn)
 
     # Re-set txn information
     did = DID()
 
-    ## Sender
+    # Sender
     obj_msg["sen"] = "cb"
 
-    ## Method
+    # Method
     obj_msg["method"] = "1"
 
-    ## Receiver
+    # Receiver
     obj_msg["rev"] = data["user"]
 
-    ## Enseed
+    # Enseed
     obj_msg["txn"] = ""
 
-    ## Address
+    # Address
     obj_msg["address"] = ""
 
     # API key
