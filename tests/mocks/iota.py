1fedad4021ec92bb12c76cabfc719d278052f8e4
HappyFeet07 <jack0978239920@gmail.com>
2020-05-18 12:56:53 +0800

    repaste iota mock

--- tests/mocks/iota.py
+++ tests/mocks/iota.py
@@ -1,20 +1,26 @@
 class Bundle():
     def __init__(self, hash=None):
-        if hash == None:
+        if hash is None:
             hash = "RUBXZTNOIYV9EULYYHKEEAHFFQPYMPZIXLVYNPHGBDBDDZERDSJHJAMGOIHHGILZGDYNP9ANJANPHZYDX"
-            self.hash = hash
+        self.hash = hash
+
+
 class IotaMock():
-    def __init__(self, node_url, seed = None):
+    def __init__(self, node_url, seed=None):
         self.url = node_url
         self.seed = seed
-        
+
     def send_transfer(self, depth, transfers, min_weight_magnitude):
         return {
-            "bundle" : Bundle()
+            "bundle": Bundle()
         }
 
     def find_transactions(self, bundles):
-        if bundles == ["RUBXZTNOIYV9EULYYHKEEAHFFQPYMPZIXLVYNPHGBDBDDZERDSJHJAMGOIHHGILZGDYNP9ANJANPHZYDX"]:
-            return {"hashes": ["KYVEAQJKIYSNH9SGULNSPDFGULCV9DETKWIFUUH9GICWMMYJFRVUSDWFNXIAXBHW9BNNXOKGVOMKZ9999"]}
+        if bundles == [
+                "RUBXZTNOIYV9EULYYHKEEAHFFQPYMPZIXLVYNPHGBDBDDZERDSJHJAMGOIHHGILZGDYNP9ANJANPHZYDX"]:
+            return {"hashes": [
+                "KYVEAQJKIYSNH9SGULNSPDFGULCV9DETKWIFUUH9GICWMMYJFRVUSDWFNXIAXBHW9BNNXOKGVOMKZ9999"]}
+
+
 def iota_mock(node_url, seed=None):
     return IotaMock(node_url, seed)
