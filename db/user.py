60f4854657d5101cee1c8e0545bd738492aff344
Bill Hsu <bl33234679@gmail.com>
2020-07-01 14:12:29 +0800

    Add user function in db

---
+++ db/user.py
@@ -0,0 +1,20 @@
+from . import connect
+import json
+import psycopg2.extras
+
+def insert(obj):
+    db = connect.connectdb()
+    c = db.cursor()
+    c.execute("""INSERT INTO users (hash, username, created_at, description, password)
+              VALUES (%s, %s, %s, %s, %s);""", (obj["hash"], obj["username"], obj["created_at"], obj["description"], obj["password"]))
+    db.commit()
+    db.close()
+
+
+def select_by_timestamp(start, end):
+    db = connect.connectdb()
+    c = db.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+    c.execute("SELECT * FROM users WHERE created_at >= (%s) AND created_at <= (%s) ORDER BY created_at;", (start, end))
+    ret = c.fetchall()
+    db.close()
+    return ret
