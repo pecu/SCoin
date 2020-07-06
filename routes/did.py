ec51b6d063c5324ce724084986a0ee24dc69d5e2
Bill Hsu <bl33234679@gmail.com>
2020-07-06 13:01:51 +0800

    Add did blueprint

---
+++ routes/did.py
@@ -0,0 +1,26 @@
+from flask import request, Blueprint
+from app.did import DID
+from app.blockchain.tangle import find_transaction_message
+
+did_blueprint = Blueprint('did', __name__)
+
+@did_blueprint.route('/new_did', methods=['POST'])
+def new_did():
+    if request.method == 'POST':
+        data = request.get_json()
+        x_api_key = request.headers.get('X-API-key')
+        
+        did = DID()
+        hash_bundle = did.new_did(x_api_key, data)
+
+        return str(hash_bundle)
+
+@did_blueprint.route('/did', methods=['GET'])
+def did():
+    if request.method == 'GET':
+        hash_did = request.args.get('hash')
+
+        ## Read from blockchain
+        content = find_transaction_message(hash_did)
+
+    return content
